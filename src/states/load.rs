6df14c2eb8847d5237de2c25ff3d437dcd88ad97
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2020-06-21 15:17:39 +0300

    update test_level layers

--- src/states/load.rs
+++ src/states/load.rs
@@ -74,6 +74,7 @@ fn load_level(world: &mut World, mut progress_counter: &mut ProgressCounter) {
     let backgrounds_map = [
         ("z-10_1.png", -10.0f32),
         ("z-10_2.png", -10.0f32),
+        ("z-15.png", -15.0f32),
         ("z-20_1.png", -20.0f32),
         ("z-20_2.png", -20.0f32),
         ("z-20_3.png", -20.0f32),
